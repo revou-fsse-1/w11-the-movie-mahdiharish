c2c7e244b34bbc1f3d84cf890437031838092169
mahdiharish <mahdiharish12@gmail.com>
2023-04-05 23:02:42 +0800

    add function to register new user

--- src/register.js
+++ src/register.js
@@ -58,4 +58,65 @@ document
   .getElementById("passwordConfirm")
   .addEventListener("input", checkConfirmPassword);
 
+const API_USER_ENDPOINT = "http://localhost:3000/users";
+
+// REGISTER NEW USER TO API USER ENDPOINT
+let registerNewUser = async () => {
+  try {
+    const response = await fetch(API_USER_ENDPOINT, {
+      method: "POST",
+      body: JSON.stringify({
+        username: `${document.getElementById("registerUsername").value}`,
+        email: `${document.getElementById("registerEmail").value}`,
+        password: `${document.getElementById("registerPassword").value}`,
+        id: ``,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    });
+    const user = await response.json();
+    console.log(user);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 // REGISTER NEW USER
+const usernameInput = document.getElementById("registerUsername");
+const emailInput = document.getElementById("registerEmail");
+const passwordInput = document.getElementById("registerPassword");
+document
+  .getElementById("registerBtn")
+  .addEventListener("click", async function (e) {
+    e.preventDefault();
+    const username = usernameInput.value;
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    try {
+      const response = await fetch(API_USER_ENDPOINT);
+      const users = await response.json();
+      const isUserRegistered = users.some((user) => user.username === username);
+      if (isUserRegistered) {
+        document.getElementById("userValidation").innerHTML =
+          "User is already registered!";
+        return;
+      }
+      const newUser = { username, email, password };
+      const createUserResponse = await fetch(API_USER_ENDPOINT, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUser),
+      });
+      if (!createUserResponse.ok) {
+        throw new Error("Failed to create user");
+      }
+      window.location.href = "./index.html";
+    } catch (error) {
+      console.error(error);
+      document.getElementById("userValidation").innerHTML =
+        "Failed to register user. Please try again later.";
+    }
+  });
